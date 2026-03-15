4ef610bc893673ae6be12f89ed4cd575364a43ad
happyladysauce <13452552349@163.com>
2026-03-15 17:27:56 +0800

    Add PostgreSQL and Redis configuration to user service
    - Introduced PostgreSQL and Redis connection settings in user.yaml and config.go.
    - Established database and Redis client connections in serviceContext.go, ensuring proper error handling for connection failures.

--- services/user/internal/config/config.go
+++ services/user/internal/config/config.go
@@ -4,4 +4,12 @@ import "github.com/zeromicro/go-zero/zrpc"
 
 type Config struct {
 	zrpc.RpcServerConf
+
+	// PostgreSQL
+	PostgreSQLDSN string `json:"PostgreSQLDSN"`
+
+	// Redis
+	RedisHost     string `json:"RedisHost"`
+	RedisPass     string `json:"RedisPass"`
+	RedisDB       int    `json:"RedisDB"`
 }
--- services/user/internal/svc/serviceContext.go
+++ services/user/internal/svc/serviceContext.go
@@ -1,13 +1,43 @@
 package svc
 
-import "github.com/HappyLadySauce/Beehive-M/services/user/internal/config"
+import (
+	"context"
+
+	"github.com/HappyLadySauce/Beehive-M/services/user/internal/config"
+	"github.com/redis/go-redis/v9"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 type ServiceContext struct {
 	Config config.Config
+
+	DB	*gorm.DB
+
+	Redis *redis.Client
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+
+	db, err := gorm.Open(postgres.Open(c.PostgreSQLDSN), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to PostgreSQL: " + err.Error())
+	}
+
+	redis := redis.NewClient(&redis.Options{
+		Addr:     c.RedisHost,
+		Password: c.RedisPass,
+		DB:       c.RedisDB,
+	})
+
+	_, err = redis.Ping(context.Background()).Result()
+	if err != nil {
+		panic("failed to connect to Redis: " + err.Error())
+	}
+
 	return &ServiceContext{
 		Config: c,
+		DB:     db,
+		Redis:  redis,
 	}
 }
